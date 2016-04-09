0c8824100f194360e4d4c79dbf33f58cad4da7ee
Val Packett <val@packett.cool>
2016-04-09 18:41:33 +0300

    eslint and broccoli

--- microformat-editor.js
+++ microformat-editor.js
@@ -7,15 +7,15 @@ Polymer({
 		item: { type: Object, value: { type: [ 'h-entry' ], properties: {} } },
 	},
 
-	getPropKeys(item) {
+	getPropKeys (item) {
 		return Object.keys(item.properties)
 	},
 
-	getPropValues(e, key) {
+	getPropValues (e, key) {
 		return e.base.properties[key]
 	},
 
-	addProp(e) {
+	addProp (e) {
 		let name = this.$['new-prop-name'].value
 		if (name.length < 1) return
 		if (name in this.item.properties) {
@@ -26,40 +26,40 @@ Polymer({
 		this.$['new-prop-name'].value = ''
 	},
 
-	removeProp(e) {
+	removeProp (e) {
 		let props = {}
 		Object.assign(props, this.item.properties)
 		delete props[e.model.key]
 		this.set('item.properties', props)
 	},
 
-	addPropValue(e) {
+	addPropValue (e) {
 		// have to ~replace~ ~the~ ~array~, not push into the existing one. because, idk, computers
 		// and the .map(x => x) clone prevents polymer from binding the new field to both the new element and the previous one
 		// this should just be a `this.push` but polymer binding sucks for now
-		this.set('item.properties.' + e.model.key, this.item.properties[e.model.key].map(x => x).concat(['']))
+		this.set('item.properties.' + e.model.key, this.item.properties[e.model.key].map((x) => x).concat(['']))
 	},
 
-	removePropValue(e) {
+	removePropValue (e) {
 		let index = e.model.index
 		let key = e.currentTarget.dataset.key
 		this.item.properties[key].splice(index, 1)
-		this.set('item.properties.' + key, this.item.properties[key].map(x => x))
+		this.set('item.properties.' + key, this.item.properties[key].map((x) => x))
 	},
 
-	isString(val) {
+	isString (val) {
 		return typeof val === 'string'
 	},
 
-	isContentHtml(val) {
+	isContentHtml (val) {
 		return typeof val.html === 'string'
 	},
 
-	isContentValueNotHtml(val) {
-		return typeof val.html !== 'string' && typeof val.type !== 'array' && typeof val.value === 'string'
+	isContentValueNotHtml (val) {
+		return typeof val.html !== 'string' && !Array.isArray(val.type) && typeof val.value === 'string'
 	},
 
-	isMicroformat(val) {
+	isMicroformat (val) {
 		return (val.type || []).length >= 1
 	},
 
