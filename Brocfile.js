17c3e6455557de0307b8e462b1ba5d2adeab0a30
Val Packett <val@packett.cool>
2016-06-03 12:54:08 +0300

    update stuff

--- Brocfile.js
+++ Brocfile.js
@@ -13,7 +13,14 @@ var root
 root = new Find('.', { include: ['*.{js,html}', 'bower_components/**/*'], exclude: ['Brocfile.js'] })
 root = new Babel(root, { ignore: ['bower_components/*'], compact: true })
 root = new Merge([
-	new Vulcanize(root, { input: 'micro-panel.html', output: 'micro-panel.html', stripComments: true }),
+	new Vulcanize(root, {
+		input: 'micro-panel.html',
+		output: 'micro-panel.html',
+		excludes: ['bower_components\/codemirror'],
+		stripComments: true,
+		inlineScripts: true
+	}),
+	new Find('./bower_components/codemirror/**/*'),
 	new Find('./bower_components/{es6-promise,webcomponentsjs}/*.min.js'),
 	new Find('./bower_components/fetch/*.js'),
 	new Find('index.html'),
