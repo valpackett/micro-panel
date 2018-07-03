6201e3af36d4724bc65b62c8bc5b4684269ff79d
Greg V <greg@unrelenting.technology>
2018-07-03 23:12:31 +0300

    micropub fetch, codeflask for html/markdown

--- devserver.js
+++ devserver.js
@@ -8,5 +8,20 @@ polka()
 		const content = fs.readFileSync('demo.html')
 		return res.end(content)
 	})
+	.get('/fake-micropub', (req, res) => {
+		res.setHeader('Content-Type', 'application/json')
+		return res.end(JSON.stringify({
+			type: ['h-entry'],
+			properties: {
+				name: ['Welcome to micro-panel'],
+				content: [
+					{
+						markdown: `This is a set of Web Components for editing IndieWeb sites.
+Look, the post source is **different**! This is a *demo*, so the micropub endpoint is fake :)`
+					}
+				],
+			},
+		}))
+	})
 	.listen(3003)
 	.then(_ => console.log('Running on localhost:3003'))
