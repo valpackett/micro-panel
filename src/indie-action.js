f3f0762566a4adf5fa1501b401e1a0edff1133e1
Val Packett <val@packett.cool>
2018-07-04 00:13:41 +0300

    Add indie-action

---
+++ src/indie-action.js
@@ -0,0 +1,22 @@
+import { mpe } from './util.js'
+
+function matchingKey (action) {
+	switch (action) {
+		case 'like': return 'like-of'
+		case 'repost': return 'repost-of'
+		case 'bookmark': return 'bookmark-of'
+		default: return 'in-reply-to'
+	}
+}
+
+export default class IndieAction extends HTMLElement {
+	connectedCallback () {
+		for (const el of this.querySelectorAll('a, button')) {
+			el.addEventListener('click', e => mpe().newEntry({
+				[matchingKey(this.getAttribute('do'))]: [this.getAttribute('with')]
+			}))
+		}
+	}
+}
+
+customElements.define('indie-action', IndieAction)
--- src/micro-panel-all.js
+++ src/micro-panel-all.js
@@ -1,3 +1,4 @@
 import './micro-panel-editor.js'
 import './micro-panel-toolbar.js'
 import './micro-panel-action.js'
+import './indie-action.js'
