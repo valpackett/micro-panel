4cc6fb2ce3d6fdc79445a536a096038b121e0f23
Greg V <greg@unrelenting.technology>
2018-07-04 23:37:40 +0300

    Add JSON editing

--- src/micro-panel-editor-entry.js
+++ src/micro-panel-editor-entry.js
@@ -1,13 +1,24 @@
 import 'codeflask-element'
 import 'prismjs/components/prism-markdown.min.js'
+import 'prismjs/components/prism-json.min.js'
 import { LitElement, html } from '@polymer/lit-element'
 import { sharedStyles, icons, iconCode } from './util.js'
 import produce from 'immer'
 
 export default class MicroPanelEditorEntry extends LitElement {
-	static get properties () { return { entry: Object, setEntry: Function } }
+	static get properties () {
+		return {
+			entry: Object, setEntry: Function, openJsonEditors: Object, jsonParseError: Object
+		}
+	}
 
-	_render ({ entry }) {
+	constructor () {
+		super()
+		this.openJsonEditors = {}
+		this.jsonParseError = {}
+	}
+
+	_render ({ entry, openJsonEditors, jsonParseError }) {
 		return html`
 			${sharedStyles}
 			<style>
@@ -33,7 +44,7 @@ export default class MicroPanelEditorEntry extends LitElement {
 				.input-row + .input-row {
 					padding-top: 0;
 				}
-				.input-row input, .input-row textarea, .input-row code-flask {
+				.input-row input, .input-row textarea, .input-row code-flask, .input-row .error-value {
 					flex: 1;
 				}
 				.input-row button {
@@ -45,6 +56,14 @@ export default class MicroPanelEditorEntry extends LitElement {
 				textarea, code-flask {
 					resize: vertical;
 					min-height: 200px;
+				}
+				.error-value {
+					color: #bb1111;
+				}
+				.json-error {
+					background: #bb1111;
+					color: #fff;
+					padding: 0.5rem;
 				}
 
 				@media screen and (min-width: 700px) {
@@ -59,18 +78,23 @@ export default class MicroPanelEditorEntry extends LitElement {
 						<button on-click=${_ =>
 							this._modify(entry, draft => delete draft.properties[propname])
 						} title="Delete this property" class="icon-button">${iconCode(icons.minus)}</button>
+						<button on-click=${_ => {
+							this.openJsonEditors = produce(openJsonEditors, x => { x[propname] = !(x[propname] || false) })
+							this.jsonParseError = produce(jsonParseError, pes => { pes[propname] = null })
+						}} title="Edit this property as JSON" class="icon-button">${iconCode(icons.json)}</button>
 						<button on-click=${_ =>
 							this._modify(entry, draft => draft.properties[propname].push(''))
 						} title="Add new value to this property" class="icon-button">${iconCode(icons.plus)}</button>
 					</header>
-					${entry.properties[propname] && entry.properties[propname].map((propval, idx) => html`
+					${openJsonEditors[propname] ? this._jsonEditor(entry, propname, jsonParseError)
+						: (entry.properties[propname] && entry.properties[propname].map((propval, idx) => html`
 						<div class="input-row">
 							${this._rowEditor(entry, propname, propval, idx)}
 							<button on-click=${_ =>
 								this._modify(entry, draft => draft.properties[propname].splice(idx, 1))
 							} title="Delete this value" class="icon-button">${iconCode(icons.minus)}</button>
 						</div>
-					`)}
+					`))}
 				</fieldset>
 			`)}
 
@@ -89,8 +113,11 @@ export default class MicroPanelEditorEntry extends LitElement {
 				}/>
 			`
 		}
+		if (propval === null) {
+			return html`<div class="error-value">null</div>`
+		}
 		if (typeof propval !== 'object') {
-			return `Unsupported object type ${typeof propval}`
+			return html`<div class="error-value">Item of unsupported type ${typeof propval}</div>`
 		}
 		if ('html' in propval) {
 			return html`
@@ -104,7 +131,28 @@ export default class MicroPanelEditorEntry extends LitElement {
 					this._modify(entry, draft => draft.properties[propname][idx].markdown = e.target.value)
 				}></code-flask>
 			`
+		} else {
+			return html`<div class="error-value">Unsupported object with keys: ${Object.keys(propval)}</div>`
 		}
+	}
+
+	_jsonEditor (entry, propname, jsonParseError) {
+		return html`
+			<code-flask language="json" value=${JSON.stringify(entry.properties[propname], null, 2)} on-value-changed=${e =>
+				this._modify(entry, draft => {
+					try {
+						draft.properties[propname] = JSON.parse(e.target.value)
+						this.jsonParseError = produce(jsonParseError, pes => { pes[propname] = null })
+					} catch (e) {
+						this.jsonParseError = produce(jsonParseError, pes => { pes[propname] = e.toString() })
+					}
+				})
+			}></code-flask>
+			${jsonParseError[propname] ? html`<div class="json-error">
+				<p><strong>JSON parsing error!</strong> The changes are not saved when this error is present. Please fix the syntax in the editor above. The error is:</p>
+				<p><code>${jsonParseError[propname]}</code></p>
+			</div>` : ''}
+		`
 	}
 
 	addNewProp (e, entry) {
